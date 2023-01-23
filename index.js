4c03f6a546c8e99377903cc41bdbbf9a7f495442
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-23 15:09:16 +0530

    node

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-// index.js    
-// http://localhost:3000/
-
-const express = require('express')
+const express = require('express');
 const app = express();
+const portNumber = 3003;
 
-app.get('/', function (req, res) {
+// app.listen(arg, arg2);
+
+app.listen(() => {
+    console.log(`Visit http://localhost:${portNumber} to view output`);
+}, portNumber);
+
+
+app.get('/', (req, resp) => {
     console.log('hello');
-  res.send('Hello World');
-})
+    resp.send('Hello world!');
+});
+    
 
-app.get('/about', function (req, res) {
-    console.log('about');
-  res.send('About company');
-})
 
-app.get('/contact', function (req, res) {
-    console.log('contact');
-  res.send('Contact us');
-})
 
-app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+// // index.js
+// // http://localhost:3000/
+
+// const express = require('express')
+// const app = express();
+
+// app.get('/', function (req, res) {
+//     console.log('hello');
+//   res.send('Hello World');
+// })
+
+// app.get('/about', function (req, res) {
+//     console.log('about');
+//   res.send('About company');
+// })
+
+// app.get('/contact', function (req, res) {
+//     console.log('contact');
+//   res.send('Contact us');
+// })
+
+// app.listen(3000);
