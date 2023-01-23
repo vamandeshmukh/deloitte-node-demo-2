a59fddab9aca22ef87c5f0ed4d126e6f7107e19f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-23 15:00:56 +0530

    node

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const express = require('express')
 const app = express();
 
 app.get('/', function (req, res) {
+    console.log('hello');
   res.send('Hello World');
+})
+
+app.get('/about', function (req, res) {
+    console.log('about');
+  res.send('About company');
+})
+
+app.get('/contact', function (req, res) {
+    console.log('contact');
+  res.send('Contact us');
 })
 
 app.listen(3000);
